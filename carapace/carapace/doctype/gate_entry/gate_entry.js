b90976a8b7ee40c9b8aa119a4448539fc1f3e0e6
Your Name <you@example.com>
2018-12-29 14:33:54 +0530

    Added Gate Entry Module

---
+++ carapace/carapace/doctype/gate_entry/gate_entry.js
@@ -0,0 +1,40 @@
+// Copyright (c) 2018, frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Gate Entry', {
+	refresh: function(frm) {
+
+	}
+});
+
+frappe.ui.form.on('Gate Entry', {
+	"material_against_po_no": function(frm) {
+		frm.set_value("po_no","");
+		if (frm.doc.po_no == ""){
+			cur_frm.clear_table("gate_entry_items");
+	               	frm.refresh_field("gate_entry_items");
+		}
+	}
+});
+
+cur_frm.add_fetch("po_no","transaction_date","po_date")
+cur_frm.add_fetch("po_no_manual","transaction_date","po_date")
+
+frappe.ui.form.on("Gate Entry", {
+    "po_no": function(frm) {
+	if (frm.doc.po_no!= ""){
+        	frappe.model.with_doc("Purchase Order", frm.doc.po_no, function() {
+		cur_frm.clear_table("gate_entry_items");
+            		var tabletransfer= frappe.model.get_doc("Purchase Order", frm.doc.po_no)
+            		$.each(tabletransfer.items, function(index, row){
+                		var d = frm.add_child("gate_entry_items");
+				console.log(row.item_code)
+                		d.item_code = row.item_code;
+				d.item_name = row.item_name;
+				d.description = row.description;
+                	frm.refresh_field("gate_entry_items");
+            });
+        });
+    }
+}
+});
